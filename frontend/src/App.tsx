bc1ede2bbb92688aa6f24d0bc6c29e6209f6767a
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-07 16:00:28 -0300

    feat: component menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 // component
 import About from "./about";
+import Menu from "./template/Menu";
 import Todo from "./todo";
 
 export function App() {
   return (
-    <>
-      <Todo />
+    <div className="container">
+      <Menu />
       <About />
-    </>
+      <Todo />
+    </div>
   );
 }
