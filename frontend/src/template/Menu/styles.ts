18e3f7aa0b20e675104bf50df0f9e315f8e5b445
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-09 07:53:30 -0300

    feat: add style navbar

---
+++ frontend/src/template/Menu/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: var(--navBar);
+  padding: 1rem 0.7rem;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+    
+    font-size: 1.4rem;
+    font-weight: 500;
+
+    font-family: 'Ubuntu', sans-serif;
+    color: var(--white-050);
+
+    margin-bottom: 0;
+    
+    svg {
+      g:first-child {
+        fill: var(--blue);
+      }
+      g {
+        fill: var(--white-050);
+      }
+      
+      width: 22px;
+      height: 22px;
+    }
+  }
+  
+  button {
+    color: var(--blue);
+    font-size: 1.2rem;
+    font-family: 'Ubuntu', sans-serif;
+    font-style: italic;
+    &:hover {
+      color: var(--blue-hover);
+    }
+  }
+`;
