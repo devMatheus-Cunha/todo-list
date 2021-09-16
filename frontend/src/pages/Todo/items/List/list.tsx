70d58f12aa29b23d2703359f7dcc537cc15f61e6
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-16 08:39:27 -0300

    feat: value for page List

--- frontend/src/pages/Todo/items/List/list.tsx
+++ frontend/src/pages/Todo/items/List/list.tsx
@@ -1,28 +1,76 @@
-import React from 'react';
+import React from "react";
+import { Tooltip } from "@material-ui/core";
 
-import { Container } from './styles';
+// images
+import trashImage from "../../../../assets/images/trash-2.svg";
+// import editImage from "../../../../assets/images/edit-2.svg";
 
-const List = () => {
+// styles
+import { Container } from "./styles";
+
+// interface and type
+interface IListProps {
+  dataList: ListType[];
+  handleRemove: (id: string) => void
+}
+
+type ListType = {
+  createdAt: string;
+  description: string;
+  done: boolean;
+  _id: string;
+};
+
+const List = ({ dataList, handleRemove }: any) => {
+  const list = dataList || [];
+
   return (
     <Container>
-    <table>
-      <thead>
-        <tr key="Ola">
-          <th>Título</th>
-          <th>Ação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <>
-          <tr key="ola">
-            <td>ola</td>
-            <td>ola</td>
+      <table>
+        <thead>
+          <tr key="Ola">
+            <th>Título</th>
+            <th>Data</th>
+            <th>Ação</th>
           </tr>
-        </>
-      </tbody>
-    </table>
-  </Container>
+        </thead>
+        <tbody>
+          <>
+            {list.map((list: ListType) => (
+              <tr key={list._id}>
+                <td>{list.description}</td>
+                <td>
+                  {" "}
+                  {new Intl.DateTimeFormat("pt-BR").format(
+                    new Date(list.createdAt)
+                  )}
+                </td>
+                <td className="action">
+                  <Tooltip title="Deletar" placement="top" arrow>
+                    <button
+                      type="button"
+                      onClick={() => handleRemove(list._id)}
+                    >
+                      <img src={trashImage} alt="Lixeira" />
+                    </button>
+                  </Tooltip>
+
+                  {/* <Tooltip title="Editar" placement="top" arrow>
+                    <button
+                      type="button"
+                      // onClick={() => handleOpenEditTransaction(value.id)}
+                    >
+                      <img src={editImage} alt="Editar" />
+                    </button>
+                  </Tooltip> */}
+                </td>
+              </tr>
+            ))}
+          </>
+        </tbody>
+      </table>
+    </Container>
   );
-}
+};
 
 export default List;
