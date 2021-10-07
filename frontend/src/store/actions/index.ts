6bcfb4f66eedd960fdb60a3d1b29d984f782780c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-07 19:15:31 -0300

    feat: actions for List

--- frontend/src/store/actions/index.ts
+++ frontend/src/store/actions/index.ts
@@ -1,6 +1,20 @@
+import axios from "axios";
+
+// axios
+const URL = "http://localhost:3003/api/todos";
+
 export const changeDescription = (event: any) => {
   return {
     type: "DESCRIPTION_CHAGED",
     payload: event.target.value
+  }
+}
+
+export const changeList = () => {
+    const search = axios.get(`${URL}?sort=-createAt`)
+   
+  return {
+    type: "LIST_CHAGED",
+    payload: search,
   }
 }
