a42e4a4159f5fafcefb216686533a2e80c49150c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-08 15:14:43 -0300

    feat: add actions for ADD_DATA

--- frontend/src/store/actions/index.ts
+++ frontend/src/store/actions/index.ts
@@ -6,15 +6,23 @@ const URL = "http://localhost:3003/api/todos";
 export const changeDescription = (event: any) => {
   return {
     type: "DESCRIPTION_CHAGED",
-    payload: event.target.value
-  }
-}
+    payload: event.target.value,
+  };
+};
 
-export const changeList = () => {
-    const search = axios.get(`${URL}?sort=-createAt`)
-   
+export const search = (description: any) => {
+  const search = description ? `&description__regex=/${description}/` : "";
+  const getData =  axios.get(`${URL}?sort=-createAt${search}`)
   return {
     type: "LIST_CHAGED",
-    payload: search,
-  }
-}
+    payload: getData,
+  };
+};
+
+export const add = (description: any) => {
+  const request =  axios.post(URL, { description })
+  return {
+    type: "ADD_DATA",
+    payload: request,
+  };
+};
